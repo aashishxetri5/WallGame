a3151142db13b8a2023c06beee2a3bdd92cf34d5
Aashish Katwal <aashiskutwal5@gmail.com>
2023-09-11 22:44:00 +0545

    feat: customized how game beings

--- start.py
+++ start.py
@@ -1,6 +1,13 @@
 from wallGame_1 import rungame_1
 from wallGame_2 import rungame_2
+from game_functions import file_log
+from pygame import quit
+from sys import exit
 
 level1 = rungame_1()
-if level1:
-    level2 = rungame_2()
+if level1 == True:
+    rungame_2()
+
+file_log.close()
+quit()
+exit()
