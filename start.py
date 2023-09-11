3eee89e3579926e07c80c8cac7992aa1bb88d70c
Aashish Katwal <aashiskutwal5@gmail.com>
2023-09-11 18:39:03 +0545

    fix: starts game normally

--- start.py
+++ start.py
@@ -1,16 +1,6 @@
-import pygame, sys
+from wallGame_1 import rungame_1
+from wallGame_2 import rungame_2
 
-from wallGame_1 import *
-from wallGame_2 import *
-
-startTime = pygame.time.get_ticks()
-status = game_level1(player_x, player_y, player_speed, walls_lvl1, startTime)
-
-if status == False:
-    pygame.time.delay(2000)
-    pygame.quit()
-    sys.exit()
-
-
-
-
+level1 = rungame_1()
+if level1 is True:
+    level2 = rungame_2()
